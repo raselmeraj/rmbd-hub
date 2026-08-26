c46a3adca12aa993f14e1252468d67a1aaf3210e
raselmeraj <mmrd111989@gmail.com>
2026-08-26 21:59:37 +0600

    Add Firebase database methods for data manipulation

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,6 +1,5 @@
-// Firebase Config for rmbd-hub - REAL CONFIG with Singapore URL!
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, onValue } from "firebase/database";
+import { getDatabase, ref, set, onValue, push, update, remove } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDBfA6ydhJ-P9630RdSgoNykEhrmZB8bYI",
@@ -9,11 +8,10 @@ const firebaseConfig = {
   projectId: "rmbd-hub",
   storageBucket: "rmbd-hub.firebasestorage.app",
   messagingSenderId: "910946223345",
-  appId: "1:910946223345:web:eba6f11e076610ec9fd550",
-  measurementId: "G-CXQNN1WY8N"
+  appId: "1:910946223345:web:eba6f11e076610ec9fd550"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
-export { ref, set, onValue };
+export { ref, set, onValue, push, update, remove };
 export default app;
